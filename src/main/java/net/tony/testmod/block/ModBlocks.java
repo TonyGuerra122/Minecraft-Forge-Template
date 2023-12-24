aefa90841e6f18423b1d313425fa86e808a4d5b5
TonyGuerra122 <guerra.anthony122@gmail.com>
2023-12-24 14:20:35 -0300

    Sound Block

--- src/main/java/net/tony/testmod/block/ModBlocks.java
+++ src/main/java/net/tony/testmod/block/ModBlocks.java
@@ -15,6 +15,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.tony.testmod.TestMod;
+import net.tony.testmod.block.custom.SoundBlock;
 import net.tony.testmod.item.ModItems;
 
 public class ModBlocks {
@@ -41,8 +42,11 @@ public class ModBlocks {
     public static final RegistryObject<Block> END_STONE_SAPPHIRE_ORE = registerBlock("end_stone_sapphire_ore", 
         () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE)
         .strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+    public static final RegistryObject<Block> SOUND_BLOCK = registerBlock("sound_block", 
+        () -> new SoundBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
+    
 
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
+        private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
         return toReturn;
