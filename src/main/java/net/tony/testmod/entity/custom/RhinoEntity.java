de55e3baadabbd5e39f0bfffdd28bca4a0cfd558
TonyGuerra122 <guerra.anthony122@gmail.com>
2024-03-16 18:22:17 -0300

    Not Working: RhinoEntity

--- src/main/java/net/tony/testmod/entity/custom/RhinoEntity.java
+++ src/main/java/net/tony/testmod/entity/custom/RhinoEntity.java
@@ -3,6 +3,9 @@ package net.tony.testmod.entity.custom;
 import javax.annotation.Nullable;
 
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
@@ -20,14 +23,19 @@ import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.ai.goal.TemptGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.tony.testmod.entity.ModEntities;
+import net.tony.testmod.entity.ai.RhinoAttackGoal;
 
-public class RhinoEntity extends Animal{
+public class RhinoEntity extends Animal {
+
+    private static final EntityDataAccessor<Boolean> ATTACKING = SynchedEntityData.defineId(RhinoEntity.class,
+            EntityDataSerializers.BOOLEAN);
 
     public RhinoEntity(EntityType<? extends Animal> p_27557_, Level p_27558_) {
         super(p_27557_, p_27558_);
@@ -36,38 +44,67 @@ public class RhinoEntity extends Animal{
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
 
+    public final AnimationState attacAnimationState = new AnimationState();
+    public int attackAnimationTimeout = 0;
 
     @Override
     public void tick() {
-        if(this.level().isClientSide()){
+        if (this.level().isClientSide()) {
             setupAnimationState();
         }
     }
 
-    private void setupAnimationState(){
-        if(this.idleAnimationTimeout <= 0){
+    private void setupAnimationState() {
+        if (this.idleAnimationTimeout <= 0) {
             this.idleAnimationTimeout = this.random.nextInt(40) + 80;
             this.idleAnimationState.start(this.tickCount);
-        }else{
+        } else {
             --this.idleAnimationTimeout;
+        }
+
+        if (this.isAttacking() && attackAnimationTimeout <= 0) {
+            attackAnimationTimeout = 80;
+            attacAnimationState.start(this.tickCount);
+        } else {
+            --this.attackAnimationTimeout;
+        }
+
+        if (!this.isAttacking()) {
+            attacAnimationState.stop();
         }
     }
 
     @Override
     protected void updateWalkAnimation(float p_268283_) {
         float f;
-        if(this.getPose() == Pose.STANDING){
+        if (this.getPose() == Pose.STANDING) {
             f = Math.min(p_268283_ * 6F, 1f);
-        }else{
+        } else {
             f = 0f;
         }
 
         this.walkAnimation.update(f, 0.2f);
     }
 
+    public void setAttacking(boolean attacking) {
+        this.entityData.set(ATTACKING, attacking);
+    }
+
+    public boolean isAttacking() {
+        return this.entityData.get(ATTACKING);
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(ATTACKING, false);
+    }
+
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
+
+        this.goalSelector.addGoal(1, new RhinoAttackGoal(this, 1.0D, true));
 
         this.goalSelector.addGoal(1, new BreedGoal(this, 1.150));
         this.goalSelector.addGoal(2, new TemptGoal(this, 1.20, Ingredient.of(Items.COOKED_BEEF), false));
@@ -77,17 +114,17 @@ public class RhinoEntity extends Animal{
         this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 3f));
         this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
 
-
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
     }
 
-    public static AttributeSupplier.Builder createAttributes(){
+    public static AttributeSupplier.Builder createAttributes() {
         return Animal.createLivingAttributes()
-            .add(Attributes.MAX_HEALTH, 200)
-            .add(Attributes.FOLLOW_RANGE, 240)
-            .add(Attributes.MOVEMENT_SPEED, 0.250)
-            .add(Attributes.ARMOR_TOUGHNESS, 0.1f)
-            .add(Attributes.ATTACK_KNOCKBACK, 0.5f)
-            .add(Attributes.ATTACK_DAMAGE, 2f);
+                .add(Attributes.MAX_HEALTH, 200)
+                .add(Attributes.FOLLOW_RANGE, 240)
+                .add(Attributes.MOVEMENT_SPEED, 0.250)
+                .add(Attributes.ARMOR_TOUGHNESS, 0.1f)
+                .add(Attributes.ATTACK_KNOCKBACK, 0.5f)
+                .add(Attributes.ATTACK_DAMAGE, 2f);
     }
 
     @Nullable
@@ -118,5 +155,5 @@ public class RhinoEntity extends Animal{
     protected SoundEvent getDeathSound() {
         return SoundEvents.DOLPHIN_DEATH;
     }
-    
+
 }
